4a94c6b6b5409cd072a65d2b6cd64ea90118f86f
snowsonz <snowsonz@163.com>
2018-03-30 03:30:52 +0800

    封装

--- src/qrcode/generate/QRCodeGenerationTest.java
+++ src/qrcode/generate/QRCodeGenerationTest.java
@@ -1,94 +1,10 @@
 package qrcode.generate;
 
-import org.apache.poi.ss.usermodel.Workbook;
+import qrcode.generate.utils.BatchQRCodeGenerateUtils;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-import qrcode.generate.bean.ShopInfo;
-import qrcode.generate.utils.CodeHelper;
-import qrcode.generate.utils.ExcelParseTool;
-import qrcode.generate.utils.QRCodeUtil;
-
-public class QRCodeGenerationTest{
-    private static final String KFC_MAIN = "http://mall.kfc.com.cn/kfcmall20/wechat/pr-con.do" +
-            "?activityId=SMK20170910715_10000&utm_source=Offline&utm_medium=DM&" +
-            "utm_campaign=BFMonthlyCard&utm_content";
-    private static final String KFC_ZJS = "http://mall.kfc.com.cn/kfcmall20/wechat/pr-con.do" +
-            "?activityId=SMK20170910731_10000&utm_source=Offline&utm_medium=DM&" +
-            "utm_campaign=DeliveryMC&utm_content=";
-    private static final String BASE_PATH = "E:\\result\\qrcode";
-    private static final String PATH_KFC_MAIN = "\\kfc_main";
-    private static final String PATH_KFC_ZJS = "\\kfc_zjs";
-    private static final String SUFFIX_FILE = ".png";
-    private static boolean qrCodeDir;
-
-    public static void main(String[] args){
-        ExcelParseTool excelParseTool = new ExcelParseTool();
-
-        excelParseTool.setFilePath("E:\\result\\res\\早餐明细.xls");
-//        excelParseTool.setFilePath("E:\\result\\res\\宅急送.xls");
-        try{
-            Workbook workbook = excelParseTool.initWorkBook();
-
-            List<ShopInfo> outData = new ArrayList<>();
-
-            if(workbook != null){
-                excelParseTool.parseWorkbook(workbook,outData);
-            }
-
-            List<ShopInfo> unique = new ArrayList<>();
-
-            for(ShopInfo shopInfo : outData){
-                if(!unique.contains(shopInfo)){
-                    unique.add(shopInfo);
-                }else{
-                    System.out.println("该名称重复：" + shopInfo.toString());
-                }
-            }
-
-            if(unique.size()>0){
-                generatorQrCode(unique,PATH_KFC_MAIN);
-            }else{
-                System.out.println("excel is empty....");
-            }
-        }catch(IOException e){
-            System.out.println(e.toString());
-        }catch(Exception e){
-            System.out.println("case: " + e.getCause().toString());
-            System.out.println("msg: " + e.getMessage());
-            e.printStackTrace();
-        }
-    }
-
-    private static void generatorQrCode(List<ShopInfo> unique,String childDir) throws Exception{
-        System.out.println("size: " + unique.size());
-        System.out.println("------------------generate QRCode started------------------");
-        System.out.println();
-        for(ShopInfo item : unique){
-            String shopId = item.getId();
-            String shopName = item.getName();
-            if(CodeHelper.isEmpty(shopId) || CodeHelper.isEmpty(shopName)){
-                continue;
-            }
-            File file = new File(BASE_PATH + childDir,shopName + "_" + shopId + SUFFIX_FILE);
-            if(!file.getParentFile().exists()){
-                qrCodeDir = file.getParentFile().mkdirs();
-            }
-            if(file.exists()){
-                System.out.println("\"该名称重复：\"" + shopName);
-                file.delete();
-            }
-            String content = KFC_MAIN + shopId;
-            System.out.println("generate " + shopName + " QRCode started ...");
-            System.out.println(content);
-            QRCodeUtil.encode(content,new FileOutputStream(file));
-            System.out.println("generate " + shopName + " QRCode completed...");
-        }
-        System.out.println();
-        System.out.println("------------------generate QRCode completed------------------");
+public class QRCodeGenerationTest {
+    public static void main(String[] args) {
+        BatchQRCodeGenerateUtils.generateQRCode("E:\\workspace\\qrcode\\src\\qrcode" +
+                "\\generate\\config\\config.properties");
     }
 }
---
+++ src/qrcode/generate/utils/BatchQRCodeGenerateUtils.java
@@ -0,0 +1,107 @@
+package qrcode.generate.utils;
+
+import org.apache.poi.ss.usermodel.Workbook;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import qrcode.generate.bean.ShopInfo;
+
+/**
+ * @Author: Snowson
+ * @Date: 2018/3/30 3:03
+ * @Description: 批量生成二维码
+ */
+
+public class BatchQRCodeGenerateUtils {
+    public static void generateQRCode(String filePath) {
+        Properties props = new Properties();
+        try {
+            //配置资源路径
+            props.load(new InputStreamReader(new FileInputStream(new File(filePath)),
+                    "utf-8"));
+            String baseUrl = props.getProperty("BASE_URL");
+            String resPath = props.getProperty("RES_PATH");
+            String outputPath = props.getProperty("OUTPUT_PATH");
+            String suffixFile = props.getProperty("SUFFIX_FILE");
+
+            //读取数据
+            List<ShopInfo> unique = new ArrayList<>();
+            List<ShopInfo> srcData = new ArrayList<>();
+            List<ShopInfo> invalidData = new ArrayList<ShopInfo>();
+            ExcelParseTool excelParseTool = new ExcelParseTool();
+            excelParseTool.setFilePath(resPath);
+
+            Workbook workbook = excelParseTool.initWorkBook();
+            if (workbook != null) {
+                excelParseTool.parseWorkbook(workbook, srcData);
+            }
+            for (ShopInfo shopInfo : srcData) {
+                if (!(CodeHelper.isEmpty(shopInfo.getId())
+                        || CodeHelper.isEmpty(shopInfo.getName()))
+                        && !unique.contains(shopInfo)) {
+                    unique.add(shopInfo);
+                } else {
+                    invalidData.add(shopInfo);
+                    System.out.println("该名称重复：" + shopInfo.toString());
+                }
+            }
+
+            if (unique.size() <= 0) {
+                System.out.println("excel is empty....");
+                return;
+            }
+
+            //批量生成二维码
+            System.out.println("size: " + unique.size());
+            System.out.println("------------------generate QRCode started------------------");
+            System.out.println();
+            //输出合法数据
+            for (ShopInfo item : unique) {
+                String shopId = item.getId();
+                String shopName = item.getName();
+                File file = new File(outputPath, shopName + "_" + shopId + suffixFile);
+                if (!file.getParentFile().exists()) {
+                    file.getParentFile().mkdirs();
+                }
+                String content = baseUrl + shopId;
+                System.out.println();
+                System.out.println("generate " + shopName + " QRCode started ...");
+                System.out.println(content);
+                QRCodeUtil.encode(content, new FileOutputStream(file));
+                System.out.println("generate " + shopName + " QRCode completed...");
+            }
+            System.out.println();
+            System.out.println("------------------generate QRCode completed------------------");
+            if (invalidData.size() <= 0) {
+                return;
+            }
+            //输出不合法的数据
+            System.out.println();
+            System.out.println();
+            System.out.println("size: " + invalidData.size());
+            System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!invalid data start!!!!!!!!!!!!!!!!!!!!!!!!!!");
+            for (ShopInfo anInvalidData : invalidData) {
+                System.out.println();
+                String id = anInvalidData.getId();
+                String name = anInvalidData.getName();
+                System.out.println("ID: " + (CodeHelper.isEmpty(id) ? "" : id));
+                System.out.println("Name: " + (CodeHelper.isEmpty(name) ? "" : name));
+            }
+            System.out.println();
+            System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!invalid data start!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("load config.properties error...");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
